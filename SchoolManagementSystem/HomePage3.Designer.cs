939ba3c2270331aac66ee334db187725fb440809
MilindaRanawaka <milindaranawaka@gmail.com>
2019-08-29 07:52:58 +0530

    HOme03

--- SchoolManagementSystem/HomePage3.Designer.cs
+++ SchoolManagementSystem/HomePage3.Designer.cs
@@ -37,6 +37,22 @@
             this.MngStuImgBtn = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.btnLogOutHeader = new System.Windows.Forms.Button();
+            this.btnExamMheader = new System.Windows.Forms.Button();
+            this.btnStudentMHeader = new System.Windows.Forms.Button();
+            this.btnResourseMHeader = new System.Windows.Forms.Button();
+            this.btnInventoryMHeader = new System.Windows.Forms.Button();
+            this.UsrlinkLabel = new System.Windows.Forms.LinkLabel();
+            this.usrImg = new System.Windows.Forms.PictureBox();
+            this.ButtonPanel = new System.Windows.Forms.Panel();
+            this.NHomeBtn = new System.Windows.Forms.Button();
+            this.imgPanel = new System.Windows.Forms.PictureBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.panel5.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.usrImg)).BeginInit();
+            this.ButtonPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.imgPanel)).BeginInit();
             this.SuspendLayout();
             // 
             // MngLibBtn
@@ -44,7 +60,7 @@
             this.MngLibBtn.BackColor = System.Drawing.SystemColors.MenuHighlight;
             this.MngLibBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.MngLibBtn.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.MngLibBtn.Location = new System.Drawing.Point(745, 329);
+            this.MngLibBtn.Location = new System.Drawing.Point(581, 515);
             this.MngLibBtn.Name = "MngLibBtn";
             this.MngLibBtn.Size = new System.Drawing.Size(145, 70);
             this.MngLibBtn.TabIndex = 37;
@@ -56,7 +72,7 @@
             // 
             this.MngLibImgBtn.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("MngLibImgBtn.BackgroundImage")));
             this.MngLibImgBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.MngLibImgBtn.Location = new System.Drawing.Point(770, 223);
+            this.MngLibImgBtn.Location = new System.Drawing.Point(606, 409);
             this.MngLibImgBtn.Name = "MngLibImgBtn";
             this.MngLibImgBtn.Size = new System.Drawing.Size(95, 92);
             this.MngLibImgBtn.TabIndex = 36;
@@ -68,7 +84,7 @@
             this.MngExmBtn.BackColor = System.Drawing.SystemColors.MenuHighlight;
             this.MngExmBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.MngExmBtn.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.MngExmBtn.Location = new System.Drawing.Point(515, 329);
+            this.MngExmBtn.Location = new System.Drawing.Point(811, 271);
             this.MngExmBtn.Name = "MngExmBtn";
             this.MngExmBtn.Size = new System.Drawing.Size(145, 70);
             this.MngExmBtn.TabIndex = 29;
@@ -80,7 +96,7 @@
             // 
             this.MngExmImgBtn.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("MngExmImgBtn.BackgroundImage")));
             this.MngExmImgBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.MngExmImgBtn.Location = new System.Drawing.Point(542, 223);
+            this.MngExmImgBtn.Location = new System.Drawing.Point(838, 165);
             this.MngExmImgBtn.Name = "MngExmImgBtn";
             this.MngExmImgBtn.Size = new System.Drawing.Size(95, 92);
             this.MngExmImgBtn.TabIndex = 28;
@@ -91,7 +107,7 @@
             this.MngStuBtn.BackColor = System.Drawing.SystemColors.MenuHighlight;
             this.MngStuBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.MngStuBtn.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.MngStuBtn.Location = new System.Drawing.Point(286, 329);
+            this.MngStuBtn.Location = new System.Drawing.Point(582, 271);
             this.MngStuBtn.Name = "MngStuBtn";
             this.MngStuBtn.Size = new System.Drawing.Size(145, 70);
             this.MngStuBtn.TabIndex = 27;
@@ -102,7 +118,7 @@
             // 
             this.MngStuImgBtn.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("MngStuImgBtn.BackgroundImage")));
             this.MngStuImgBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.MngStuImgBtn.Location = new System.Drawing.Point(310, 223);
+            this.MngStuImgBtn.Location = new System.Drawing.Point(606, 165);
             this.MngStuImgBtn.Name = "MngStuImgBtn";
             this.MngStuImgBtn.Size = new System.Drawing.Size(95, 92);
             this.MngStuImgBtn.TabIndex = 26;
@@ -112,7 +128,7 @@
             // 
             this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
             this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.button1.Location = new System.Drawing.Point(993, 223);
+            this.button1.Location = new System.Drawing.Point(838, 409);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(95, 92);
             this.button1.TabIndex = 38;
@@ -123,12 +139,152 @@
             this.button2.BackColor = System.Drawing.SystemColors.MenuHighlight;
             this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(974, 329);
+            this.button2.Location = new System.Drawing.Point(810, 515);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(145, 70);
             this.button2.TabIndex = 39;
             this.button2.Text = "View Marks";
             this.button2.UseVisualStyleBackColor = false;
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.Color.SkyBlue;
+            this.panel5.Controls.Add(this.button3);
+            this.panel5.Controls.Add(this.btnLogOutHeader);
+            this.panel5.Controls.Add(this.btnExamMheader);
+            this.panel5.Controls.Add(this.btnStudentMHeader);
+            this.panel5.Controls.Add(this.btnResourseMHeader);
+            this.panel5.Controls.Add(this.btnInventoryMHeader);
+            this.panel5.Location = new System.Drawing.Point(17, 7);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(1228, 92);
+            this.panel5.TabIndex = 54;
+            // 
+            // btnLogOutHeader
+            // 
+            this.btnLogOutHeader.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.btnLogOutHeader.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLogOutHeader.Font = new System.Drawing.Font("Times New Roman", 11F, System.Drawing.FontStyle.Bold);
+            this.btnLogOutHeader.ForeColor = System.Drawing.Color.Maroon;
+            this.btnLogOutHeader.Location = new System.Drawing.Point(1078, 20);
+            this.btnLogOutHeader.Name = "btnLogOutHeader";
+            this.btnLogOutHeader.Size = new System.Drawing.Size(145, 50);
+            this.btnLogOutHeader.TabIndex = 25;
+            this.btnLogOutHeader.Text = "Log Out";
+            this.btnLogOutHeader.UseVisualStyleBackColor = true;
+            // 
+            // btnExamMheader
+            // 
+            this.btnExamMheader.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.btnExamMheader.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnExamMheader.Font = new System.Drawing.Font("Times New Roman", 8F, System.Drawing.FontStyle.Bold);
+            this.btnExamMheader.Location = new System.Drawing.Point(348, 20);
+            this.btnExamMheader.Name = "btnExamMheader";
+            this.btnExamMheader.Size = new System.Drawing.Size(145, 50);
+            this.btnExamMheader.TabIndex = 1;
+            this.btnExamMheader.Text = "My Profile";
+            this.btnExamMheader.UseVisualStyleBackColor = false;
+            // 
+            // btnStudentMHeader
+            // 
+            this.btnStudentMHeader.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.btnStudentMHeader.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnStudentMHeader.Font = new System.Drawing.Font("Times New Roman", 8F, System.Drawing.FontStyle.Bold);
+            this.btnStudentMHeader.Location = new System.Drawing.Point(523, 20);
+            this.btnStudentMHeader.Name = "btnStudentMHeader";
+            this.btnStudentMHeader.Size = new System.Drawing.Size(145, 50);
+            this.btnStudentMHeader.TabIndex = 18;
+            this.btnStudentMHeader.Text = "Exam";
+            this.btnStudentMHeader.UseVisualStyleBackColor = false;
+            // 
+            // btnResourseMHeader
+            // 
+            this.btnResourseMHeader.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.btnResourseMHeader.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnResourseMHeader.Font = new System.Drawing.Font("Times New Roman", 8F, System.Drawing.FontStyle.Bold);
+            this.btnResourseMHeader.Location = new System.Drawing.Point(891, 20);
+            this.btnResourseMHeader.Name = "btnResourseMHeader";
+            this.btnResourseMHeader.Size = new System.Drawing.Size(145, 50);
+            this.btnResourseMHeader.TabIndex = 21;
+            this.btnResourseMHeader.Text = "View Marks";
+            this.btnResourseMHeader.UseVisualStyleBackColor = false;
+            // 
+            // btnInventoryMHeader
+            // 
+            this.btnInventoryMHeader.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.btnInventoryMHeader.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnInventoryMHeader.Font = new System.Drawing.Font("Times New Roman", 8F, System.Drawing.FontStyle.Bold);
+            this.btnInventoryMHeader.Location = new System.Drawing.Point(701, 20);
+            this.btnInventoryMHeader.Name = "btnInventoryMHeader";
+            this.btnInventoryMHeader.Size = new System.Drawing.Size(145, 50);
+            this.btnInventoryMHeader.TabIndex = 19;
+            this.btnInventoryMHeader.Text = "Appointment";
+            this.btnInventoryMHeader.UseVisualStyleBackColor = false;
+            // 
+            // UsrlinkLabel
+            // 
+            this.UsrlinkLabel.AutoSize = true;
+            this.UsrlinkLabel.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.UsrlinkLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UsrlinkLabel.Location = new System.Drawing.Point(61, 272);
+            this.UsrlinkLabel.Name = "UsrlinkLabel";
+            this.UsrlinkLabel.Size = new System.Drawing.Size(171, 25);
+            this.UsrlinkLabel.TabIndex = 53;
+            this.UsrlinkLabel.TabStop = true;
+            this.UsrlinkLabel.Text = "Milinda Ranawaka";
+            // 
+            // usrImg
+            // 
+            this.usrImg.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.usrImg.Image = ((System.Drawing.Image)(resources.GetObject("usrImg.Image")));
+            this.usrImg.Location = new System.Drawing.Point(58, 122);
+            this.usrImg.Name = "usrImg";
+            this.usrImg.Size = new System.Drawing.Size(174, 135);
+            this.usrImg.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.usrImg.TabIndex = 52;
+            this.usrImg.TabStop = false;
+            // 
+            // ButtonPanel
+            // 
+            this.ButtonPanel.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ButtonPanel.Controls.Add(this.NHomeBtn);
+            this.ButtonPanel.Location = new System.Drawing.Point(22, 330);
+            this.ButtonPanel.Name = "ButtonPanel";
+            this.ButtonPanel.Size = new System.Drawing.Size(260, 335);
+            this.ButtonPanel.TabIndex = 51;
+            // 
+            // NHomeBtn
+            // 
+            this.NHomeBtn.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.NHomeBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NHomeBtn.ForeColor = System.Drawing.Color.Red;
+            this.NHomeBtn.Location = new System.Drawing.Point(11, 6);
+            this.NHomeBtn.Name = "NHomeBtn";
+            this.NHomeBtn.Size = new System.Drawing.Size(235, 35);
+            this.NHomeBtn.TabIndex = 24;
+            this.NHomeBtn.Text = "Home";
+            this.NHomeBtn.UseVisualStyleBackColor = false;
+            // 
+            // imgPanel
+            // 
+            this.imgPanel.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.imgPanel.Location = new System.Drawing.Point(22, 104);
+            this.imgPanel.Name = "imgPanel";
+            this.imgPanel.Size = new System.Drawing.Size(260, 220);
+            this.imgPanel.TabIndex = 50;
+            this.imgPanel.TabStop = false;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Times New Roman", 8F, System.Drawing.FontStyle.Bold);
+            this.button3.Location = new System.Drawing.Point(164, 21);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(145, 50);
+            this.button3.TabIndex = 55;
+            this.button3.Text = "Home";
+            this.button3.UseVisualStyleBackColor = false;
             // 
             // HomePage3
             // 
@@ -136,6 +292,11 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.GrayText;
             this.ClientSize = new System.Drawing.Size(1262, 673);
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.UsrlinkLabel);
+            this.Controls.Add(this.usrImg);
+            this.Controls.Add(this.ButtonPanel);
+            this.Controls.Add(this.imgPanel);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.MngLibBtn);
@@ -149,7 +310,12 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Welcome Student";
             this.Load += new System.EventHandler(this.HomePage3_Load);
+            this.panel5.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.usrImg)).EndInit();
+            this.ButtonPanel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.imgPanel)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -162,5 +328,17 @@
         private System.Windows.Forms.Button MngStuImgBtn;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Button btnLogOutHeader;
+        private System.Windows.Forms.Button btnExamMheader;
+        private System.Windows.Forms.Button btnStudentMHeader;
+        private System.Windows.Forms.Button btnResourseMHeader;
+        private System.Windows.Forms.Button btnInventoryMHeader;
+        private System.Windows.Forms.LinkLabel UsrlinkLabel;
+        private System.Windows.Forms.PictureBox usrImg;
+        private System.Windows.Forms.Panel ButtonPanel;
+        private System.Windows.Forms.Button NHomeBtn;
+        private System.Windows.Forms.PictureBox imgPanel;
+        private System.Windows.Forms.Button button3;
     }
 }

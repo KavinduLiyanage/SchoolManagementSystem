e32a39f30b81781400fb5545a561df0b43979bcc
ransa <ransa@DESKTOP-F01AG7U>
2019-09-26 04:00:03 +0530

    Library Error Fix

--- SchoolManagementSystem/LibraryAddBook.cs
+++ SchoolManagementSystem/LibraryAddBook.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data.SqlClient;
 using System.Collections;
-//nhjb
+
 namespace SchoolManagementSystem
 {
     public partial class LibraryAddBook : Form
